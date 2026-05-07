f9f059fdf9a11b5fbd7c052508e21908ee71bdff
AhmedMohamedHabashi <habashy123456789a@gmail.com>
2026-05-07 19:44:59 +0300

    refactor: Remove unused import for product list view in section_title.dart

--- lib/feature/home/presentation/widgets/section_title.dart
+++ lib/feature/home/presentation/widgets/section_title.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fresco/config/routes/app_routes.dart';
 import 'package:fresco/core/utils/colors/app_colors.dart';
 import 'package:fresco/core/utils/text_style/app_text_style.dart';
-import 'package:fresco/feature/product_list/presentation/views/product_list.dart';
 import 'package:go_router/go_router.dart';
 
 class SectionTitle extends StatelessWidget {
